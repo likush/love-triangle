29e6f57d143e9f631f4e6ed7c869e3fcc2524a39
lkush <l.kushnariova@yandex.ru>
2018-02-23 16:44:31 +0300

    Task implementation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    preferences.unshift('');
+    for (let i = 0; i < preferences.length; i++) {
+        let firstEl = preferences[i]; // i = 1, firstEl = 2
+        let secondEl = preferences[firstEl]; // secondEl = 3
+        let thirdEl = preferences[secondEl]; // thirdEl = 1
+        if (i === thirdEl
+            && preferences[firstEl] !== firstEl
+            && preferences[secondEl] !== secondEl
+            && preferences[thirdEl] !== thirdEl) {
+            count++;
+            debugger
+        }
+    }
+    return count/3;
 };
